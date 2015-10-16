0328298203c2f09588cb5092a88c1beb7cd22c73
yuki514kk <kksahiyuko.5148901@gmail.com>
2015-10-16 18:42:38 +0900

    Mapクラスの枠組みを用意した

---
+++ Assets/Map.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Otoge.Util
+{
+  public class Map
+  {
+    private static class SubExtensions
+    {
+      public const string Easy = "ez";
+      public const string Medium = "med";
+      public const string Hard = "hd";
+      public const string Expert = "ex";
+    }
+
+    public const string Extension = "otm";
+    public const string DirPath = "";
+    public string MapDirPath;
+    public string MapFilePath;
+
+    public void Load(string musicId, Difficulty difficulty)
+    {
+
+    }
+
+    public void Save(List<Note> notes)
+    {
+
+    }
+
+    public void Save(List<Note> notes, string musicId)
+    {
+
+    }
+
+    public Header ParseHeader()
+    {
+
+    }
+
+    public List<Note> ParseNotes()
+    {
+
+    }
+
+    public List<Command> ParseCommands()
+    {
+
+    }
+  }
+}
