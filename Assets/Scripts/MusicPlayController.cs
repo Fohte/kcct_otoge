34a887ba44605bf6853a6ce7ea76bdfc613aa4e1
ringilkt <tokitorikou@gmail.com>
2015-10-18 08:44:11 +0900

    Temporary Noteという名前のPrefabを作成

---
+++ Assets/Scripts/MusicPlayController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayController : MonoBehaviour
+{
+
+  void Start()
+  {
+    Music.Play(name);
+  }
+
+  void Update()
+  {
+  
+  }
+
+}
