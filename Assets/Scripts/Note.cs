1f184998133c59a893a31242bff84fd2f81ba015
ringilkt <tokitorikou@gmail.com>
2015-10-18 08:53:01 +0900

    1拍ごとにTemporary Noteをインスタンス化してゲーム内に生成するSetNoteParBeat関数を実装

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Note : MonoBehaviour
 {
-
+  public int x = 0;
   void Start()
   {
 
@@ -11,7 +11,17 @@ public class Note : MonoBehaviour
 
   void Update()
   {
-
+    SetNoteParBeat();
   }
 
+  void SetNoteParBeat()
+  {
+    bool changeBeat = Music.IsJustChangedBeat();
+    if (changeBeat)
+    {
+      GameObject note = (GameObject)Resources.Load("Prefabs/Temporary Note");
+      Instantiate(note, new Vector3(x, 0, 0), Quaternion.identity);
+      x++;
+    }
+  }
 }
