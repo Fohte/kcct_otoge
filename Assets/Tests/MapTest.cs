3e1a23bca8573396923dc11712ca226613f4c6b3
yuki514kk <kksahiyuko.5148901@gmail.com>
2015-10-19 18:51:51 +0900

    Map クラスの単体テストを用意

---
+++ Assets/Tests/MapTest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using Otoge.Util;
+
+public class MapTest : MonoBehaviour
+{
+  void Start()
+  {
+    var map = new Map();
+    map.Load("1", Difficulty.Easy);
+    Debug.Log(map.MapFilePath);
+  }
+}
