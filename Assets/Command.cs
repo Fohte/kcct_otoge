4158ba5756910811295765bc84f2c0a14bac755a
izayoi <qt.gsxx@outlook.jp>
2015-10-19 14:32:35 +0900

    Command クラスを作成

---
+++ Assets/Command.cs
@@ -0,0 +1,28 @@
+﻿namespace Otoge.Util
+{
+  public class Command
+  {
+    public static class Channel
+    {
+      public const string BPMSetter = "00";
+      public const string MeasureSetter = "01";
+    }
+
+    public class BPM
+    {
+      public int Bar;
+      public string Rhythm;
+      public double Value;
+    }
+
+    public class Measure
+    {
+      public int Bar;
+      public string Rhythm;
+      public int Numer;
+      public int Denom;
+    }
+
+    public const string MapPrefix = "*";
+  }
+}
