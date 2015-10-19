d27d674c2234c86d92a012ab0472572ece1a1e12
yuki514kk <kksahiyuko.5148901@gmail.com>
2015-10-19 19:07:01 +0900

    Otoge.Util.Map クラスでファイル情報を格納するように
    Otoge.Util.Map#Load(), Otoge.Util.Map#Save()
    が呼び出された際にファイルパスなどをフィールドに格納する処理を追加

--- Assets/Map.cs
+++ Assets/Map.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace Otoge.Util
@@ -21,27 +22,53 @@ namespace Otoge.Util
 
     public void Load(string musicId, Difficulty difficulty)
     {
-      
+      setFileInfo(musicId, difficulty);
     }
 
-    public void Save(Header header, Command List<Command> commands, List<Note> notes, string musicId = null, Difficulty? difficulty = null)
+    public void Save(Header header, List<Command> commands, List<Note> notes, string musicId, Difficulty difficulty)
     {
-      if (musicId == null) musicId = MusicId;
-      if (difficulty == null) difficulty = Difficulty;
+      setFileInfo(musicId, difficulty);
     }
 
     public Header ParseHeader()
     {
-
+      return null;
     }
 
     public List<Note> ParseNotes()
     {
-
+      return null;
     }
 
     public List<Command> ParseCommands()
     {
+      return null;
+    }
+
+    void setFileInfo(string musicId, Difficulty difficulty)
+    {
+      this.Difficulty = difficulty;
+      this.MusicId = musicId;
+
+      string subExtension = "";
+      switch (Difficulty)
+      {
+        case Difficulty.Easy:
+          subExtension = "ez";
+          break;
+        case Difficulty.Medium:
+          subExtension = "med";
+          break;
+        case Difficulty.Hard:
+          subExtension = "hd";
+          break;
+        case Difficulty.Expert:
+          subExtension = "ex";
+          break;
+        default:
+          break;
+      }
+      this.MapFilePath = DirPath + MusicId + "/" + MusicId + "." + subExtension + "." + Extension;
 
     }
   }
