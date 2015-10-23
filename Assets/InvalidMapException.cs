89bb6fb745d7e2b2052e29fca0ecc93fa3fe41a1
izayoi <qt.gsxx@outlook.jp>
2015-10-23 16:38:53 +0900

    Otoge.InvaidMapException の作成

---
+++ Assets/InvalidMapException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Otoge
+{
+  public class InvalidMapException : Exception
+  {
+    public InvalidMapException() : base() { }
+    public InvalidMapException(string message) : base(message) { }
+    public InvalidMapException(string message, Exception inner) : base(message, inner) { }
+  }
+}
