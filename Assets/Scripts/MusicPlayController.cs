c5498b95a2196004622ac142d7aba9064fef141a
ringilkt <tokitorikou@gmail.com>
2015-10-27 19:50:23 +0900

    譜面データを読み込んでリズムよくノートを生成できるようにした

--- Assets/Scripts/MusicPlayController.cs
+++ Assets/Scripts/MusicPlayController.cs
@@ -5,32 +5,55 @@ using System.Collections.Generic;
 public class MusicPlayController : MonoBehaviour
 {
   int startBar;
-
+  public static List<bool> createNoteThisTiming = new List<bool>();
   List<string> rhythms = new List<string>()
   {
-    "11111100",
-    "000000000111"
+    "100100100100100100101010" //"11111100" + "000000000111"                     
   };
+
+  int barNumber = 0, musicalNoteNumber = 0;
+  float X = 0,Y = 0;
+  bool timing;
+  List<int> bar = new List<int>();
+  List<int> beat = new List<int>();
+  List<int> unit = new List<int>();
 
   void Start()
   {
     Music music = GameObject.Find("music").AddComponent<Music>();
     music.DebugText = GameObject.Find("status").GetComponent<TextMesh>();
     music.Sections = new List<Music.Section>();
-    var musicSection = new Music.Section(startBar: 0);
-    musicSection.Name = "start";
-    music.Sections.Add(musicSection);
-    var musicSection2 = new Music.Section(startBar: 2);
-    musicSection2.Name = "next";
-    music.Sections.Add(musicSection2);
-    music.OnVal();
 
-    Music.Play(name, "start");
+    for (barNumber = 0; barNumber < rhythms.Count; barNumber++)
+    {
+      int UnitPerBeat = rhythms[barNumber].Length / 4;
+      int UnitPerBar = rhythms[barNumber].Length;
+
+      music.Sections.Add(new Music.Section(startBar: barNumber, mtBeat: UnitPerBeat, mtBar: UnitPerBar, tempo: 120));
+      music.Sections[barNumber].Name = "Section" + barNumber;
+      music.OnVal();
+      for (musicalNoteNumber = 0; musicalNoteNumber < rhythms[barNumber].Length; musicalNoteNumber++)
+      {
+        if (rhythms[barNumber][musicalNoteNumber] == '1')
+        {
+          bar.Add(barNumber);
+          beat.Add(musicalNoteNumber / (UnitPerBeat));
+          unit.Add(musicalNoteNumber % (UnitPerBeat));
+        }
+      }
+    }
+    Music.Play(name, "Section0");
   }
-
   void Update()
   {
-
+    for (barNumber = 0; barNumber < bar.Count; barNumber++)
+    {
+      timing = Music.IsJustChangedAt(bar: bar[barNumber], beat: beat[barNumber], unit: unit[barNumber]);
+      if (timing)
+      {
+        Note.SetNoteParBeat(X,Y); //デバッグ用に適当な変数を入れているが、本来は譜面フォーマットに入っている座標情報を解析して引数に入れる。
+        X += 0.5f; //これもデバッグ用。オブジェクトを出す位置をずらして確認しやすくするため。
+      }
+    }
   }
-
 }
--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -3,29 +3,21 @@ using System.Collections;
 
 public class Note : MonoBehaviour
 {
-
-  public int X = 0;
-  public int Y = 0;
-  public int Bar = 0;
   public string Rhythm;
 
   void Start()
   {
-
+   
   }
 
   void Update()
   {
-    SetNoteParBeat();
+
   }
 
-  void SetNoteParBeat()
+  public static void SetNoteParBeat(float X,float Y)
   {
-    if (Music.IsJustChangedBeat())
-    {
-      GameObject note = (GameObject)Resources.Load("Prefabs/Temporary Note");
-      Instantiate(note, new Vector3(X, 0, 0), Quaternion.identity);
-      X++;
-    }
+    GameObject note = (GameObject)Resources.Load("Prefabs/Temporary Note");
+    Instantiate(note, new Vector3(X, Y, 0), Quaternion.identity);
   }
 }
