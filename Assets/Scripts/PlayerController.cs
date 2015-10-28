aac8653e06bf67ace820c3449f5ef63415ef7529
Hayato KAWAI <g@fohte.net>
2015-10-28 14:57:03 +0900

    fix conflict

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using Otoge.Util;
+
+public class PlayerController : MonoBehaviour
+{
+  void Start()
+  {
+    var map = new Map("1", Difficulty.Easy);
+  }
+}
---
+++ Assets/Scripts/TitleButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleButton : MonoBehaviour
+{
+  void Start()
+  {
+    Button button = this.GetComponent<Button>();
+    button.onClick.AddListener(() =>
+    {
+      Debug.Log("Clicked");
+    });
+  }
+}
