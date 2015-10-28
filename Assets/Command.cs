eec6c45669d4b3424f318e6d2538e7da29995d33
izayoi <qt.gsxx@outlook.jp>
2015-10-28 11:54:07 +0900

    Otoge.Util.Command#ToString() の作成

--- Assets/Command.cs
+++ Assets/Command.cs
@@ -4,6 +4,7 @@ namespace Otoge.Util
 {
   public class Command
   {
+
     public static class Channel
     {
       public const string BPMSetter = "00";
@@ -14,5 +15,22 @@ namespace Otoge.Util
 
     public List<BPM> BPMs = new List<BPM>();
     public List<Measure> Measures = new List<Measure>();
+
+    public override string ToString()
+    {
+      string str = "";
+
+      foreach (var bpm in BPMs)
+      {
+        str += MapPrefix + bpm.Bar.ToString("d3") + Channel.BPMSetter + ":" + bpm.Value.ToString("f2") + ":" + bpm.Rhythm + "\n";
+      }
+
+      foreach (var measure in Measures)
+      {
+        str += MapPrefix + measure.Bar.ToString("d3") + Channel.MeasureSetter + ":" + measure.Numer.ToString("d") + "/" + measure.Denom.ToString("d") + ":" + measure.Rhythm + "\n";
+      }
+
+      return str;
+    }
   }
 }
