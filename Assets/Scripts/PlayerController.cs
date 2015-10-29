47960fc1762d42e9e0ddfe0396c517654a71f0c9
izayoi <qt.gsxx@outlook.jp>
2015-10-29 12:09:34 +0900

    Otoge.Util.PlayerController の作成

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,19 @@
 ﻿using UnityEngine;
 using Otoge.Util;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
   void Start()
   {
     var map = new Map("1", Difficulty.Easy);
+    var title = GameObject.Find("Title").GetComponent<Text>();
+    var difficulty = GameObject.Find("Difficulty").GetComponent<Text>();
+
+    title.text = map.Header.Title;
+    difficulty.text = map.Difficulty.ToString().ToUpper();
+    
+    Image image = GameObject.Find("Image").gameObject.GetComponent<Image>();
+    image.sprite = Resources.Load<Sprite>("Jackets/" + map.Header.JacketFile);
   }
 }
