ae018dad98db4f19a2fe044e3db1a6fb0d3b575f
ringilkt <tokitorikou@gmail.com>
2015-10-30 10:42:57 +0900

    MusicPlayControllerの一部の変数がlistである必要がなかったので修正した

--- Assets/Scripts/MusicPlayController.cs
+++ Assets/Scripts/MusicPlayController.cs
@@ -5,26 +5,22 @@ using Otoge.Util;
 
 public class MusicPlayController : MonoBehaviour
 {
-  public const int UnitPerBar = 16;  //デバッグ用。実際は音符の混合とがあること、拍子の変化に対応できなかったことより、unitPerBarは48、unitPerBeatは12。
+  public const int UnitPerBar = 16;  //デバッグ用。実際は音符の混合と表紙の変化がないことから、unitPerBarは48、unitPerBeatは12。
   public const int UnitPerBeat = 4;
 
+  
   public static double ElapsedTime = 0;
   int a = 0;
+  int bar = 0;
+  int beat = 0;
+  int unit = 0;
+  double mtPerSecond = 0;
   double currentBPM;
-  float X = 0, Y = 0;  
-  List<int> bars = new List<int>();
-  List<int> beats = new List<int>();
-  List<int> units = new List<int>();
-  List<double> mtPerSeconds = new List<double>();
+  List<float> posX = new List<float>();
+  List<float> posY = new List<float>();
   List<double> noteCreateTimings = new List<double>();
   List<double> noteTapTimings = new List<double>();
-  List<string> rhythms = new List<string>()
-  {
-    "1000100010101111"
-    //"000000000000100100100000",// "00 00 11 10"
-    //"100100100100100100101010" // "11 11 11 00" + "000 000 000 111"
-  };
-
+  
   void Start()
   {
     var map = new Map("1", Difficulty.Easy);
@@ -44,24 +40,28 @@ public class MusicPlayController : MonoBehaviour
       music.OnVal();
     }
 
-    //ノートを置くタイミングを決める部分。
+    //ノートを置くタイミングと座標を決める部分。
     for (int aaa = 0; aaa < map.Notes.Count; aaa++)
     {
+      double currentBPM = GetCurrentBPM(aaa);
+      mtPerSecond = UnitPerBeat * (currentBPM / 60);
       for (int bbb = 0; bbb < map.Notes[aaa].Rhythm.Length; bbb++)
       {
-        double currentBPM = GetCurrentBPM(aaa);
-        mtPerSeconds.Add(UnitPerBeat * currentBPM / 60);
-
         if (map.Notes[aaa].Rhythm[bbb] == '1')
         {
-          bars.Add(map.Notes[aaa].Bar);
-          beats.Add(bbb / (UnitPerBeat));
-          units.Add(bbb % (UnitPerBeat));
-          noteTapTimings.Add((bars[bars.Count - 1] * UnitPerBar + beats[beats.Count - 1] * UnitPerBeat + units[units.Count - 1]) / mtPerSeconds[mtPerSeconds.Count - 1] + elapsedTimeUpToThisBar); //単位は秒。
-          noteCreateTimings.Add(noteTapTimings[noteTapTimings.Count - 1] ); //1は適当に置いてるだけの数字。たたく何秒前にノーツを表示させたいかを入れる。 
+          bar = (map.Notes[aaa].Bar);
+          beat = (bbb / (UnitPerBeat));
+          unit = (bbb % (UnitPerBeat));
+          noteTapTimings.Add((beat * UnitPerBeat + unit) / mtPerSecond + elapsedTimeUpToThisBar); //単位は秒。
+          noteCreateTimings.Add(noteTapTimings[noteTapTimings.Count - 1] - 1); //1は適当に置いてるだけの数字。たたく何秒前にノーツを表示させたいかを入れる。
+          posX.Add(map.Notes[aaa].X);
+          posY.Add(map.Notes[aaa].Y);
+          Debug.Log(bar + " " + beat + " " + unit);
+          Debug.Log(noteTapTimings[noteTapTimings.Count - 1] + "秒");
         }
       }
-      elapsedTimeUpToThisBar += (map.Notes[aaa].Rhythm.Length / mtPerSeconds[aaa]);
+      elapsedTimeUpToThisBar += (map.Notes[aaa].Rhythm.Length / mtPerSecond);
+      Debug.Log(elapsedTimeUpToThisBar);
     }
     Music.Play(name, "Section0");
   }
@@ -73,8 +73,8 @@ public class MusicPlayController : MonoBehaviour
     {
       if (ElapsedTime >= noteCreateTimings[a])
       {
-        NoteManager.CreateNote(X, Y, noteTapTimings[a]); //デバッグ用に適当な変数を入れているが、本来は譜面情報に入っている座標を解析して引数に入れる。
-        X += 1; //これもデバッグ用。オブジェクトを出す位置をずらして確認しやすくするため。
+        NoteManager.CreateNote(posX[a],posY[a], noteTapTimings[a]);
+        Debug.Log(ElapsedTime + "秒");
         a++;
       }
     }
@@ -86,8 +86,15 @@ public class MusicPlayController : MonoBehaviour
     int currentBar = map.Notes[Bar].Bar;
     for (int ccc = 0; ccc < map.Command.BPMs.Count; ccc++)
     {
-      if (currentBar < map.Command.BPMs[ccc].Bar) currentBPM = map.Command.BPMs[ccc - 1].Value;
-      if (currentBar == map.Command.BPMs[ccc].Bar) currentBPM = map.Command.BPMs[ccc].Value;
+      if (currentBar < map.Command.BPMs[ccc].Bar)
+      {
+        currentBPM = map.Command.BPMs[ccc - 1].Value;
+        break;
+      }else if (currentBar == map.Command.BPMs[ccc].Bar)
+        {
+          currentBPM = map.Command.BPMs[ccc].Value;
+          break;
+        }
     }
     return currentBPM;
   }
