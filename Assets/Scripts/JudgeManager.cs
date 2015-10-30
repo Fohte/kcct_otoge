fac1b6bdc594966a78948a4636ac7cb43a161ab9
izayoi <qt.gsxx@outlook.jp>
2015-10-30 15:01:32 +0900

    Otoge.Util.JudgeManager#JudgeTiming() の修正作成

---
+++ Assets/Scripts/JudgeManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using Otoge.Util;
+
+public class JudgeManager : MonoBehaviour
+{
+  public double exactTapTiming; //単位は秒
+  double actualTapTiming; //単位は秒
+
+  void Start ()
+  {
+    
+  }
+	
+	void Update ()
+  {
+    
+	}
+
+  void OnMouseDown() //PCでのデバッグ用にクリックに反応するようにしているが、、実際にはタッチやフリックで反応するようにする。
+  {
+    JudgeTiming();
+  }
+
+  public Judge JudgeTiming()
+  {
+    actualTapTiming = MusicPlayController.elapsedTime;
+    double difference = System.Math.Abs(exactTapTiming - actualTapTiming) * 1000; //単位はミリ秒。この差がどれぐらいかによって判定する(?)
+    Destroy(gameObject);
+    if (difference <= 21)
+    {
+      return Judge.Perfect;
+    }
+    else if (difference <= 42)
+    {
+      return Judge.Great;
+    }
+    else if (difference <= 84)
+    {
+      return Judge.Good;
+    }
+    else if (difference <= 250)
+    {
+      return Judge.Bad;
+    }
+    else
+    {
+      return Judge.Miss;
+    }
+  }
+}
