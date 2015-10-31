f0be7e9182e680dd3cff5278427523869725366f
izayoi <qt.gsxx@outlook.jp>
2015-10-31 17:28:17 +0900

    TitleButton の修正

--- Assets/Scripts/TitleButton.cs
+++ Assets/Scripts/TitleButton.cs
@@ -8,7 +8,7 @@ public class TitleButton : MonoBehaviour
     Button button = this.GetComponent<Button>();
     button.onClick.AddListener(() =>
     {
-      Application.LoadLevel("MusicList");
+      Application.LoadLevel("MusicSelect");
     });
   }
 }
