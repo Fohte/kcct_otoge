7ce854d7e0b112463e2c097b47b491e10a34a8aa
ringilkt <tokitorikou@gmail.com>
2015-10-31 16:25:30 +0900

    スコアとコンボ数を取得及び表示できるようにした

--- Assets/Scripts/JudgeManager.cs
+++ Assets/Scripts/JudgeManager.cs
@@ -4,18 +4,15 @@ using Otoge.Util;
 
 public class JudgeManager : MonoBehaviour
 {
-  public const float Perfect = 1.0f;
-  public const float Great = 0.7f;
-  public const float Good = 0.5f;
-  public const float Bad = 0.1f;
-  public const float Miss = 0.0f;
+  
 
   public double ExactTapTiming = 2; //単位は秒
   double actualTapTiming; //単位は秒
+  public static Judge judge;
 
   void Start ()
   {
-    
+
   }
 	
 	void Update ()
@@ -23,20 +20,21 @@ public class JudgeManager : MonoBehaviour
     
 	}
 
-  void OnMouseDown() //PCでのデバッグ用にクリックに反応するようにしているが、、実際にはタッチやフリックで反応するようにする。
+  public void OnMouseDown() //PCでのデバッグ用にクリックに反応するようにしているが、、実際にはタッチやフリックで反応するようにする。
   {
-   if (JudgeTiming() != Judge.Miss)
+    judge = JudgeTiming();
+    Debug.Log(judge);
+    if (judge != Judge.Miss)
     {
-      Destroy(gameObject);
+     Destroy(gameObject);
     }
+    MusicPlayController.comboAndScoreUpdate();
   }
 
   public Judge JudgeTiming()
   {
     actualTapTiming = MusicPlayController.ElapsedTime;
     double difference = System.Math.Abs(ExactTapTiming - actualTapTiming) * 1000; //単位はミリ秒。この差がどれぐらいかによって判定する(?)
-    Debug.Log(actualTapTiming);
-    Debug.Log(ExactTapTiming);
     if (difference <= 42)
     {
       return Judge.Perfect;
