7f2433507b334d4e9135f0137720fcd7dfb488c8
ringilkt <tokitorikou@gmail.com>
2015-10-31 10:57:50 +0900

    ノートを押したときに判定ができるようにした

--- Assets/Scripts/JudgeManager.cs
+++ Assets/Scripts/JudgeManager.cs
@@ -4,7 +4,13 @@ using Otoge.Util;
 
 public class JudgeManager : MonoBehaviour
 {
-  public double exactTapTiming; //単位は秒
+  public const float Perfect = 1.0f;
+  public const float Great = 0.7f;
+  public const float Good = 0.5f;
+  public const float Bad = 0.1f;
+  public const float Miss = 0.0f;
+
+  public double ExactTapTiming = 2; //単位は秒
   double actualTapTiming; //単位は秒
 
   void Start ()
@@ -19,27 +25,31 @@ public class JudgeManager : MonoBehaviour
 
   void OnMouseDown() //PCでのデバッグ用にクリックに反応するようにしているが、、実際にはタッチやフリックで反応するようにする。
   {
-    JudgeTiming();
+   if (JudgeTiming() != Judge.Miss)
+    {
+      Destroy(gameObject);
+    }
   }
 
   public Judge JudgeTiming()
   {
     actualTapTiming = MusicPlayController.ElapsedTime;
-    double difference = System.Math.Abs(exactTapTiming - actualTapTiming) * 1000; //単位はミリ秒。この差がどれぐらいかによって判定する(?)
-    Destroy(gameObject);
-    if (difference <= 21)
+    double difference = System.Math.Abs(ExactTapTiming - actualTapTiming) * 1000; //単位はミリ秒。この差がどれぐらいかによって判定する(?)
+    Debug.Log(actualTapTiming);
+    Debug.Log(ExactTapTiming);
+    if (difference <= 42)
     {
       return Judge.Perfect;
     }
-    else if (difference <= 42)
+    else if (difference <= 92)
     {
       return Judge.Great;
     }
-    else if (difference <= 84)
+    else if (difference <= 166)
     {
       return Judge.Good;
     }
-    else if (difference <= 250)
+    else if (difference <= 500)
     {
       return Judge.Bad;
     }
